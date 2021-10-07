b673a7849158d2423f2755c1d4566b07aa2f16f7
Mateusz.S <mateuszsebastianowicz@gmail.com>
2021-10-07 15:28:41 +0200

    poprawna konfiguracja 2 DB

--- itech_app/schemas.py
+++ itech_app/schemas.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from sqlalchemy.sql.sqltypes import VARCHAR, SmallInteger
 from pydantic import BaseModel, EmailStr
 from typing import Optional
 
@@ -14,3 +17,33 @@ class User(BaseModel):
     can_create: bool = False
     is_admin: bool = False
     notification: bool = False
+
+class Log(BaseModel):
+    sql_query: str
+    action_data: datetime 
+
+class Program(BaseModel):
+    NrPRM: SmallInteger
+    NazwaProgramu: VARCHAR(50)
+    KodProgramu: VARCHAR(15)
+    CzyProgPrior: bool
+    CzyNiepWsad: bool
+    CzyUltraM05: bool
+    CzyPolewaczka: bool
+    KtlPMC: SmallInteger
+    SzerTraw: SmallInteger
+    Pow: SmallInteger
+    CzyOdmuch: SmallInteger
+    KtlNapPW: SmallInteger
+    KtlCzasNN: SmallInteger
+    KtlPRK: SmallInteger
+    KtlCzasWygrz: SmallInteger
+    FsfCzasSusz: SmallInteger
+    Gmp: SmallInteger
+    CzyMask: SmallInteger
+    ProPMZad: SmallInteger
+    ProKolor: VARCHAR(50)
+    ProCzyOtrzep: bool
+    ProCzasWygrz: SmallInteger
+    StRozZad: SmallInteger
+    CzyAktywny: bool
