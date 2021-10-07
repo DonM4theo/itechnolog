6106d22c9c82927d4835824410a5967f3862b66b
Mateusz.S <mateuszsebastianowicz@gmail.com>
2021-10-07 15:04:29 +0200

    poprawna konfiguracja 2 DB

--- itech_app/schemas.py
+++ itech_app/schemas.py
@@ -1,15 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from typing import Optional
 
 class User(BaseModel):
     login: str
     hashed_password: str
     first_name: str
     last_name: str
-    email: str
-    phone_number: str
+    email: EmailStr
+    phone_number: str = "UNKNOWN"
     is_active: bool
-    can_edit: bool
-    can_remove: bool
-    can_create: bool
-    is_admin: bool
-    notification: bool
+    can_edit: bool = False
+    can_remove: bool = False
+    can_create: bool = False
+    is_admin: bool = False
+    notification: bool = False
